887338d331052da98a575f59b01eddb0b5b11e67
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-03 23:20:16 -0500

    set up routes

--- front-end-client/src/App.js
+++ front-end-client/src/App.js
@@ -1,3 +1,16 @@
+// import from other libraries
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import other Components
+import Navigation from "./components/Navigation";
+import Landing from "./components/Landing";
+import Login from "./components/login_register/Login";
+import Register from "./components/login_register/Register";
+import Profile from "./components/profile/Profile";
+import Statistics from "./components/statistics/Statistics";
+import Games from "./components/games/Games";
+import Minesweeper from "./components/minesweeper/Minesweeper";
+
 // import styling
 import "./stylesheets/css/App.css";
 
@@ -5,6 +18,19 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">Hello from App</header>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/minesweeper" elements={<Minesweeper />} />
+          <Route path="/*" element={<Landing />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
