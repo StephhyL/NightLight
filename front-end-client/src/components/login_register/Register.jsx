badfe51b21a407dd73a5b7e764d48ba15ae850d8
BlesMateo <blesildaangelicamateo@gmail.com>
2022-02-05 12:50:53 -0500

    Adding useState values and setValues and creating the form for registration

--- front-end-client/src/components/login_register/Register.jsx
+++ front-end-client/src/components/login_register/Register.jsx
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
 // import styling
 import "../../stylesheets/css/Register.css";
 
 const Register = () => {
-  return <div>Hello from Register</div>;
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  return (
+    //<div>Register</div>;
+    <div className="register-form">
+      <form className="registration">
+        <input
+          className="form-field"
+          placeholder="First Name"
+          name="firstName"
+        />
+        <input className="form-field" placeholder="Last Name" name="lastName" />
+        <input className="form-field" placeholder="Email" name="email" />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
